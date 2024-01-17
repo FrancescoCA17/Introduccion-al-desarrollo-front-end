f571240008386a535eb647aa2dfdc1773a202587
FrancescoCA17 <134182627+FrancescoCA17@users.noreply.github.com>
2024-01-17 10:03:27 -0500

    Create pet.js

---
+++ Programación con JavaScript/24-01-17/pet.js
@@ -0,0 +1,15 @@
+var petDog="Rex";
+var dogSound="woof";
+var petCat="Pepper";
+
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: ",petDog);
+console.log("My pet cat's name is: " ,petCat);
+var dogSound="woof";
+var catSound="purr";
+
+console.log(petDog,"says",dogSound);
+console.log(petCat,"says",catSound);
+catSound="meow";
+console.log(petCat,"now says",catSound);
