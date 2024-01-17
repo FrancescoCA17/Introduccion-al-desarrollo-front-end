319644e0846461e0be3492d42c1268783d8db710
FrancescoCA17 <134182627+FrancescoCA17@users.noreply.github.com>
2024-01-17 12:23:43 -0500

    Create E1condiconalesbucles.js

---
+++ Programación con JavaScript/24-01-17/E1condiconalesbucles.js
@@ -0,0 +1,11 @@
+for(var i=1;i<11;i++){
+    if (i== 1){
+        console.log("Medalla de oro");
+    }else if (i== 2){
+        console.log("Medalla de plata");
+    }else if (i== 3){
+        console.log("Medalla de bronce");
+    }else{
+        console.log(i);
+    }
+}
