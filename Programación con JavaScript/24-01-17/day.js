40e3277f28bed84308cbf1a3060245128ea28b19
FrancescoCA17 <134182627+FrancescoCA17@users.noreply.github.com>
2024-01-17 11:38:12 -0500

    Create day.js

---
+++ Programación con JavaScript/24-01-17/day.js
@@ -0,0 +1,27 @@
+var day="Monday";
+switch(day){
+    case "Monday":
+        console.log(':(');
+        break;
+    case "Tuesday":
+        console.log(':/');
+        break;
+    case "Wednesday":
+        console.log(':|');
+        break;
+    case "Thursday":
+        console.log(':P');
+        break;
+    case "Friday":
+        console.log(':)');
+        break;
+    case "Saturday":
+        console.log(':D');
+        break;
+    case "Sunday":
+        console.log('Relax');
+        break;
+    default:
+        console.log('No existe tal día');
+        break;
+}
