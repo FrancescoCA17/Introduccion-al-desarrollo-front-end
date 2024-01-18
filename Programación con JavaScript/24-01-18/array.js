5f135cf6600bc73b136833c3e2d602c9670bf76e
FrancescoCA17 <134182627+FrancescoCA17@users.noreply.github.com>
2024-01-18 10:20:34 -0500

    Create array.js

---
+++ Programación con JavaScript/24-01-18/array.js
@@ -0,0 +1,10 @@
+//sintaxis literal de array
+var clothes=[];
+clothes.push('polo');
+clothes.push('jean');
+clothes.push('zapatillas');
+clothes.push('correa');
+clothes.push('medias');
+clothes.pop();
+clothes.push('boxer');
+console.log(clothes[2]);
