77f5c95b220457f49a4d5178b19596d5456c32d5
FrancescoCA17 <134182627+FrancescoCA17@users.noreply.github.com>
2024-01-22 11:45:04 -0500

    Create iteraInOf.js

---
+++ Programación con JavaScript/24-01-22/iteraInOf.js
@@ -0,0 +1,35 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+function logDairy() {
+    for (const item of dairy) {
+        console.log(item);
+    }
+}
+logDairy();
+// Task 2
+const animal = {
+
+canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (const can of Object.keys(bird)) {
+        console.log(can+":",bird[can]);
+    }
+}
+birdCan();
+// Task 3
+function animalCan() {
+    for (const ani in bird) {
+        console.log(ani+":",bird[ani]);
+    }
+}
+animalCan();
+
